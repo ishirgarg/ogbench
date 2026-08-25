92a66a40ab2a210a3b49f875274e4407e5a7b9d9
Ishir Garg <gargishir@gmail.com>
2026-08-25 16:27:37 -0700

    Use rail_gpu4_low queue for opal discrete-50skills resume
    Correcting the QOS from rail_gpu4_lowest to rail_gpu4_low.

--- impls/scripts/run_opal_discrete_50skills_resume.sh
+++ impls/scripts/run_opal_discrete_50skills_resume.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=opal_discrete_50skills_resume
 #SBATCH --account=co_rail
 #SBATCH --partition=savio4_gpu
-#SBATCH --qos=rail_gpu4_lowest
+#SBATCH --qos=rail_gpu4_low
 #SBATCH --requeue
 #SBATCH --gres=gpu:A5000:1
 #SBATCH --cpus-per-task=4
